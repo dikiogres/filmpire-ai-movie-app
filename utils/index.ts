bfd87ee56152e3c15fe1941205b28d8b7c5fb817
dikiogres <dikiogres@gmail.com>
2023-07-01 00:30:09 +0700

    feat: fetch car mutation hooks

---
+++ utils/index.ts
@@ -0,0 +1,22 @@
+export async function fetchCars() {
+  // Set the required headers for the API request
+  const headers: HeadersInit = {
+    "X-RapidAPI-Key":
+      process.env.NEXT_PUBLIC_RAPID_API_KEY ||
+      "d014eebcd9msh9d8d1a7b8a7bb5cp17b677jsn78e719474a7a",
+    "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+  };
+
+  // Set the required headers for the API request
+  const response = await fetch(
+    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars`,
+    {
+      headers: headers,
+    }
+  );
+
+  // Parse the response as JSON
+  const result = await response.json();
+
+  return result;
+}
