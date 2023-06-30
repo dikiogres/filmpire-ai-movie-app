28b0717fdc8d40b832c45f64b89288dd26cabb5d
dikiogres <dikiogres@gmail.com>
2023-07-01 01:00:11 +0700

    feat: add car card components

--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,6 @@ import Hero from "./Hero";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import CustomButton from "./CustomButton";
+import CarCard from "./CarCard";
 
-export { Hero, CustomButton, Navbar, Footer };
+export { Hero, CustomButton, Navbar, Footer, CarCard };
