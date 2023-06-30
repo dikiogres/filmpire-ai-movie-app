7fef7c6ef38f4fa6797febf881ab7ce854175a3c
dikiogres <dikiogres@gmail.com>
2023-07-01 00:19:46 +0700

    feat: search bar initiation

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+"use client";
+
+import Image from "next/image";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+import SearchManufacturer from "./SearchManufacturers";
 
 const SearchBar = () => {
-  return <div>SearchBar</div>;
+  const [manufacturer, setManuFacturer] = useState("");
+
+  return (
+    <form className="searchbar">
+      <div className="searchbar__item">
+        <SearchManufacturer
+          manufacturer={manufacturer}
+          setManuFacturer={setManuFacturer}
+        />
+      </div>
+    </form>
+  );
 };
 
 export default SearchBar;
