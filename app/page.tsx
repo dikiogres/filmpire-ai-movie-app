de46c4e8145e772cba9b9ab6f379121080b2494a
dikiogres <dikiogres@gmail.com>
2023-07-01 00:34:08 +0700

    fix: test on fetch all cars

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,13 @@ import { Hero } from "@/components";
 import CustomFilter from "@/components/CustomFilter";
 import SearchBar from "@/components/SearchBar";
 import Image from "next/image";
+import { fetchCars } from "@/utils";
 
-export default function Home() {
+export default async function Home() {
+  const allCars = await fetchCars();
+
+  console.log(allCars);
+
   return (
     <main className="overflow-hidden">
       <Hero />
